68491d142b4d0cbda512ab4d6c79faf6f2ae6084
Weier Guo <weguo@ucdavis.edu>
2023-02-22 10:31:23 -0800

    exercise chapter7

---
+++ Chapter7/R-7-2.py
@@ -0,0 +1,6 @@
+# Descrobe a good algorithm for concatenating two singly linked lists L and M, given only references to the first node of each list, into a single list L' that contains all the nodes of L followed by all the nodes of M.
+
+class LinkedQueue:
+
+    class _Node:
+        
