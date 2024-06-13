2cf206eb7cfdb0581ce0c20df9e167062c431210
guoweier <weguo@ucdavis.edu>
2024-06-13 14:28:22 -0700

    exercise chapter 8

--- Chapter8/C-8-34.py
+++ Chapter8/C-8-34.py
@@ -2,7 +2,7 @@
 
 # for each internal node, it has 3 children.
 # Case1: If T has only one node, then it is the root. So nI=0, nE=1. nE=nI*2+1
-# Case2: If T has more than one node. 
-# If one child is external, that internal node can be substitude by one external. We can actually remove that internal node together with that external node.
-# In this operation, we finally can left with a tree T only has one internal node (root), and has 3 children.
-# So nE=3, nI=1. So nE=2*nI+1
+# Case2: If T has more than one node.
+# If one child is external, that internal node can be substitude by two external. We can actually remove that internal node together with two external node.
+# In this operation, we finally can left with a tree T only has one internal node (root), and has 3 children. So if substitude root with two external nodes, then left with 1 external node.
+# In this way, nE=2*nI+1
