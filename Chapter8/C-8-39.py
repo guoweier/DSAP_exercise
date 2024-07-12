47a0d1414432841cbed5f7945415f322020bb616
guoweier <weguo@ucdavis.edu>
2024-07-12 10:24:57 -0700

    exercise chapter 8

---
+++ Chapter8/C-8-39.py
@@ -0,0 +1,1 @@
+# Add support in LinkedBinaryTree for a method, _swap(p,q), that has the effect of restructuring the tree so that the node referenced by p takes the place of the node refereced by q, and vice versa. Make sure to properly handle the case when the nodes are adjacent. 
